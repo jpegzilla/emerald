7d36845a248689f2eec3ad3c11f90814ffb3697a
Jpegzilla <jpegzilla@gmail.com>
2019-08-30 17:43:17 -0400

    service worker now removes old cache when updating

--- service-worker.js
+++ service-worker.js
@@ -5,15 +5,31 @@ const FILES_TO_CACHE = [
   "./scripts/utils.min.js"
 ];
 
-const CACHE_NAME = "emeraldCache_v0103";
+const CACHE_NAME = "emeraldCache_v0104";
+const cacheWhitelist = [CACHE_NAME];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        console.log("[ServiceWorker] Pre-caching offline page");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .then(() => {
+        caches.keys().then(keyList => {
+          return Promise.all(
+            keyList.map(key => {
+              if (cacheWhitelist.indexOf(key) === -1) {
+                console.log("[ServiceWorker] removing old key in cache: ", key);
+                return caches.delete(key);
+              }
+            })
+          );
+        });
+      })
   );
 });
 
